ba041323a85565d03d8f745404e89d2bface37bd
vicroni <drasa_tec@hotmail.com>
2018-05-16 23:53:30 -0500

    Lol

--- index.js
+++ index.js
@@ -5,14 +5,46 @@ var app = require('express')();
 var http = require('http').Server(app);
 var bodyParser =  require("body-parser");
 
+
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
+passport.use(new Strategy({
+  clientID: '172252590129742',
+  clientSecret: '2164c43945f78e9fc211b8de5747cd82',
+  callbackURL: "http://localhost:7000/auth/facebook/callback"
+},
+function(accessToken, refreshToken, profile, cb) {
+  return cb(null, profile);
+}));
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 /*
  *	Configuracion de las rutas
  */ 
 const facebook_login = require('./routes/facebook_login');
 app.get('/facebook_login', facebook_login.get);
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    console.log(res.user)
+    res.redirect('/');
+});
+
+
 app.get('/condiciones', facebook_login.condiciones);
 
 const academias = require('./routes/academias');
--- routes/facebook_login.js
+++ routes/facebook_login.js
@@ -1,6 +1,17 @@
-exports.get = function (req, res) {
-    res.end('/condiciones.html');
-};
 exports.condiciones = function (req, res) {
     res.sendFile(__dirname+ '/condiciones.html');
+};
+
+
+exports.get = function (req, res) {
+    passport.use(new FacebookStrategy({
+        clientID: '172252590129742',
+        clientSecret: '2164c43945f78e9fc211b8de5747cd82',
+        callbackURL: "http://localhost:7000/auth/facebook/callback"
+      },
+      function(accessToken, refreshToken, profile, cb) {
+        console.log(accessToken, refreshToken, profile, cb);
+        return cb(profile);
+      }
+    ));
 };
